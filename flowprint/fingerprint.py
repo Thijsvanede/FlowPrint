fabfa54887e746e71784d1f7456f98fc13807b50
Thijs van Ede <t.s.vanede@utwente.nl>
2020-02-05 17:50:31 -0800

    Cleaned Fingerprint

--- flowprint/fingerprint.py
+++ flowprint/fingerprint.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 class Fingerprint(frozenset):
 
     def __new__(cls, *args):
@@ -22,12 +20,6 @@ class Fingerprint(frozenset):
         set_self  = self .certificates | self .destinations
         set_other = other.certificates | other.destinations
         return len(set_self & set_other) / max(len(set_self | set_other), 1)
-
-    def contains(self, other):
-        """Returns if flow is in Fingerprint."""
-        set_self  = self .certificates | self .destinations
-        set_other = other.certificates | other.destinations
-        return len(set_self & set_other) / min(len(set_self), len(set_other))
 
     ########################################################################
     #                         Property definitions                         #
@@ -67,41 +59,6 @@ class Fingerprint(frozenset):
         # Return cached certificates
         return self._certificates
 
-    @property
-    def labels(self):
-        """Get all labels related to this Fingerprint."""
-        # Initialise destinations
-        labels = set()
-        # Loop over all clusters
-        for cluster in self:
-            labels |= set(cluster.labels.keys())
-
-        # Return result
-        return labels
-
-    @property
-    def label(self):
-        """Get the label of this Fingerprint."""
-        if len(self) == 0:
-            return -1
-        else:
-            votes = dict()
-            for cluster in self:
-                vote = cluster.labels.most_common(1)[0][0]
-                votes[vote] = votes.get(vote, 0) + 1
-            return max(votes.items(), key=lambda x: x[1])[0]
-
-    @property
-    def time_start(self):
-        """Get the start time of flows in fingerprint."""
-        return min(min(c.samples).time_start() for c in self)
-
-    @property
-    def time_end(self):
-        """Get the start time of flows in fingerprint."""
-        return max(max(c.samples).time_end() for c in self)
-
-
     ########################################################################
     #                            Cast to object                            #
     ########################################################################
@@ -109,34 +66,6 @@ class Fingerprint(frozenset):
     def as_set(self):
         """Get fingerprint as a set."""
         return frozenset(self.certificates | self.destinations)
-
-    ########################################################################
-    #                             I/O methods                              #
-    ########################################################################
-
-    def show(self):
-        """String representation of fingerprint."""
-        destinations = ",\n    ".join(str(d) for d in sorted(self.destinations))
-        certificates = ",\n    ".join(str(c) for c in sorted(self.certificates))
-        labels       = ", ".join(str(l) for l in sorted(self.labels))
-        size_dsts    = len(self)
-        size_flows   = sum(len(c.samples) for c in self)
-        print("""Fingerprint {{{}}} [size={}, flows={}]
-  Started: {}
-  Ended  : {}
-----------------------------------------
-  Destinations={{
-    {}
-  }}
-  Certificates={{
-    {}
-  }}""".format(labels if size_flows else "Empty", size_dsts, size_flows,
-    datetime.fromtimestamp(self.time_start).strftime("%d-%m-%Y %H:%M:%S.%f") if size_flows else "-",
-    datetime.fromtimestamp(self.time_end).strftime(  "%d-%m-%Y %H:%M:%S.%f") if size_flows else "-",
-    destinations, certificates))
-
-    def __str__(self):
-        return "Fingerprint({}) {}".format(hex(id(self)), self.labels)
 
     ########################################################################
     #                              Overrides                               #
