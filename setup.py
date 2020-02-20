71a2e61aeb41a733d7628d0002784dfcdbcf6b64
Thijs van Ede <t.s.vanede@utwente.nl>
2020-02-20 13:46:24 -0800

    Next version added

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flowprint",
-    version="0.0.2",
+    version="0.0.3",
     author="Thijs van Ede",
     author_email="t.s.vanede@utwente.nl",
     description="FlowPrint: Semi-Supervised Mobile-App Fingerprinting on "
