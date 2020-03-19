0c110ab806fe51ff134c7029caa2bc62c6be92e2
Thijsvanede <t.s.vanede@utwente.nl>
2020-03-18 21:38:08 -0700

    Set entry point to index

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,5 +53,8 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# -- Set master doc ----------------------------------------------------------
+master_doc = 'index'
+
 # -- Mock modules ------------------------------------------------------------
 autodoc_mock_imports = ['cryptography', 'matplotlib', 'networkx', 'numpy', 'pandas', 'pyshark', 'sklearn']
