5d916e326c03e84c839bd50348cf4695b53d5108
Thijsvanede <t.s.vanede@utwente.nl>
2020-03-18 21:35:34 -0700

    Mock modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,3 +52,6 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Mock modules ------------------------------------------------------------
+autodoc_mock_imports = ['cryptography', 'matplotlib', 'networkx', 'numpy', 'pandas', 'pyshark', 'sklearn']
