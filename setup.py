e52629c04a8ca5dd7934c40258de25b5da3f440f
Thijsvanede <t.s.vanede@utwente.nl>
2020-05-05 11:26:20 -0700

    FlowPrint new version: added export of CrossCorrelationGraph

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flowprint",
-    version="1.0.0",
+    version="1.0.1",
     author="Thijs van Ede",
     author_email="t.s.vanede@utwente.nl",
     description="FlowPrint: Semi-Supervised Mobile-App Fingerprinting on "
