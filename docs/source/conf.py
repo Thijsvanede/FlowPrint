c2d117f7c52501cda2fb70821442dc66280ab9ab
Thijsvanede <t.s.vanede@utwente.nl>
2020-05-07 17:19:02 -0700

    Added source code links to Reference

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.coverage', 'sphinx_rtd_theme']
+extensions = ['recommonmark', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.coverage', 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
